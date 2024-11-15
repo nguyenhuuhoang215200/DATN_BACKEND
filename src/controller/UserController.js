25f377615fc2d8ee8001aa1f8eacc666eec15d9d
nguyenhuuhoang215200 <nhhoang215200@gmail.com>
2024-11-15 20:02:54 +0700

    Cập nhật mã nguồn ngày 15/11

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,22 +1,52 @@
 import UserService from "../service/UserService";
 const handleLogin = async (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-    console.log (email,'và' ,password);
-    if (!email || !password) {
-        return res.status(500).json({
-            errcode: 1,
-            message: 'Missing inputs parameter'
-        })
-    }
-    let UserData = await UserService.handleUserLogin(email, password);
-    return res.status(200).json({
-        errcode: UserData.error,
-        message: UserData.errmessage,
-        user : UserData.user ? UserData.user :{}
+  let email = req.body.email;
+  let password = req.body.password;
+  console.log(email, "và", password);
+  if (!email || !password) {
+    return res.status(500).json({
+      errcode: 1,
+      message: "Missing inputs parameter",
     });
-}
-
+  }
+  let UserData = await UserService.handleUserLogin(email, password);
+  return res.status(200).json({
+    errcode: UserData.error,
+    message: UserData.errmessage,
+    user: UserData.user ? UserData.user : {},
+  });
+};
+const handleGetAllUser = async (req, res) => {
+  try {
+    // Gọi service để lấy danh sách người dùng
+    let UserID = req.query.id;
+    console.log(UserID);
+    if (!UserID) {
+      return res.status(200).json({
+        errcode: 1,
+        errmessage: "ID rỗng",
+        users: [],
+      });
+    }
+    // Trả về phản hồi thành công với dữ liệu người dùng
+    else {
+      const users = await UserService.getAllUser(UserID);
+      res.status(200).json({
+        errcode: 0,
+        errmessage: "ok",
+        users,
+      });
+    }
+  } catch (error) {
+    // Xử lý và trả về lỗi nếu có vấn đề xảy ra
+    return res.status(500).json({
+      errcode: 1,
+      errmessage: "Internal server error",
+      error: error.message,
+    });
+  }
+};
 export default {
-    handleLogin
-}
+  handleLogin,
+  handleGetAllUser,
+};
--- src/route/web.js
+++ src/route/web.js
@@ -1,19 +1,20 @@
 import express from "express";
-import homeController from '../controller/homeController'
-import UserController from '../controller/UserController'
+import homeController from "../controller/homeController";
+import UserController from "../controller/UserController";
 let route = express.Router();
 
 let initWebroutes = (app) => {
-    route.get('/', homeController.getDataCrud)
-    route.get('/createCRUD', homeController.getCRUD)
-    route.post('/post-crud', homeController.postCRUD)
-    route.get('/readCRUD', homeController.getDataCrud)
-    route.get('/deleteUser/:id', homeController.getDeleteUser)
-    route.post('/delete/:id', homeController.postDeleteUser)
-    route.get('/updateUser/:id', homeController.updateUser)
-    route.post('/postUpdateCRUD/:id', homeController.PostupdateUser)
+  route.get("/", homeController.getDataCrud);
+  route.get("/createCRUD", homeController.getCRUD);
+  route.post("/post-crud", homeController.postCRUD);
+  route.get("/readCRUD", homeController.getDataCrud);
+  route.get("/deleteUser/:id", homeController.getDeleteUser);
+  route.post("/delete/:id", homeController.postDeleteUser);
+  route.get("/updateUser/:id", homeController.updateUser);
+  route.post("/postUpdateCRUD/:id", homeController.PostupdateUser);
 
-    route.post('/api/login',UserController.handleLogin)
-    return app.use("/", route);
-}
-module.exports = initWebroutes 
+  route.post("/api/login", UserController.handleLogin); //Login
+  route.get("/api/get-all-user", UserController.handleGetAllUser); //Lấy tất cả user or 1
+  return app.use("/", route);
+};
+module.exports = initWebroutes;
